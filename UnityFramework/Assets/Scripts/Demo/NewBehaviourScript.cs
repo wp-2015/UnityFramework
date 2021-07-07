77fadb3753019ed8217280e1f17a95d534a68954
王鹏 <wp>
2021-07-07 17:14:14 +0800

    Demo

--- UnityFramework/Assets/Scripts/Demo/NewBehaviourScript.cs
+++ UnityFramework/Assets/Scripts/Demo/NewBehaviourScript.cs
@@ -8,12 +8,20 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        NetworkManager.Connect("Main", "127.0.0.1", 8999);
     }
 
     // Update is called once per frame
     void Update()
     {
         NetworkManager.Update();
+    }
+
+    private void OnGUI()
+    {
+        if(GUILayout.Button("Send", GUILayout.Width(100), GUILayout.Height(100)))
+        {
+            ChatProxy.Instance.SendInt(100);
+        }
     }
 }
